17fab34d88900dbfb2fdf8ecef936ed996f04e84
Ravi <ravi.veeraghanta@gmail.com>
2025-11-07 17:37:38 +0530

    esbuild  instead of terser for minification

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: '/',
   build: {
     target: 'esnext',
-    minify: 'terser',
+    minify: 'esbuild',
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false
